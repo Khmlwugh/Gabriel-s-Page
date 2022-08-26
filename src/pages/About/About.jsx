c502d3be134ce7f8a185e73360dc38334d116b6c
Gabo <gaabf@hotmail.com>
2022-08-25 21:09:32 -0500

    ADD: added About page components

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,17 @@
 import Header from "../../components/Header";
+import Details from "./components/Details";
+import Profile from "./components/Profile";
+import { AboutContainer } from "./styles/styles";
 
 function About() {
 
   return(
     <>
       <Header/>
-      <div>
-        About me
-      </div>
+      <AboutContainer>
+        <Profile/>
+        <Details/>
+      </AboutContainer>
     </>
   )
 }
